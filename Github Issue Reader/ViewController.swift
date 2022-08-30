47d40e311f2a0b1026d3fe80b2735644971647d7
Savannah Sosa <savannah.e.sosa@gmail.com>
2022-08-30 14:22:12 -0400

    First commit

---
+++ Github Issue Reader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Github Issue Reader
+//
+//  Created by Savannah Sosa on 8/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

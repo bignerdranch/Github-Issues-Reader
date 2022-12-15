5423f90623b30df6deaf5aacf32b4d3b1cb65efe
Alivia (Via) <vfairchild@bignerdranch.com>
2022-12-14 19:31:22 -0700

    IssueModel and UserModel comments added

--- Github Issue Reader/Models/IssueModel.swift
+++ Github Issue Reader/Models/IssueModel.swift
@@ -9,26 +9,35 @@ import Foundation
 import UIKit
 
 // 1 hour
+
+// the data within the struct is going to need to conform to the protocols codable and hashable so we take those types in at the beginning.
 struct Issue: Codable, Hashable {
+    // every constant below is pulled from the JSON data model in the API most of the data is required in the API so it's okay that they're non-optional. Only optional is the "body" property because it could return a nil
     let id: Int
     let title: String
+    // state data has more than just a type to return which is handled in the enum below
     let state: State
+    // User is data that has even more data within it so it's broken out into it's own model to handle that info
     let user: User
     let body: String?
     let createdAt: String
-
+    //
     enum State: String, Codable, Hashable {
+        // Is the issue closed or open? That's what we are asking the state to represent.
         case open
         case closed
-
+        // wtf
         var localizedTitle: String {
             rawValue.capitalized
         }
     }
 }
 
+// this extension is telling the state cases to be assigned a specific color.
 extension UIColor {
+    // static keeps the function only accessible to this file. if Issue gets called elsewhere in the project this function will not be available to modify.
     static func from(state: Issue.State) -> UIColor {
+        // when the state is case - return -
         switch state {
         case .open:
             return .green
--- Github Issue Reader/Models/UserModel .swift
+++ Github Issue Reader/Models/UserModel .swift
@@ -8,7 +8,9 @@
 import Foundation
 
 // 2 mins
+// the data within the struct is going to need to conform to the protocols codable and hashable so we take those types in at the beginning.
 struct User: Codable, Hashable {
+    // simply pulling in the JSON data and translating it to Swift again here. the avatarURL May or may not have a value so it's an optional. 
     let id: Int
     let login: String
     let avatarURL: String?
